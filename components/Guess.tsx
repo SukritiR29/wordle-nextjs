1c7ec36ab0f7c033d5bf4e38506dcba8fd7a8148
Sukriti <87890532+SukritiR29@users.noreply.github.com>
2023-03-01 12:16:36 +0530

    addition of arrays and functions

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -1,3 +1,21 @@
-export default function Guess() {
-    return <div>guess</div>
+export default function Guess({isGuessed, guess, word}) {
+    return (
+    <div className="grid grid-cols-5 gap-5 mb-2">
+        {
+            new Array(5).fill(0).map((_, i) => {
+                const bgColor = !isGuessed 
+                  ? 'bg-black' 
+                  : guess[i] === word[i] 
+                  ? 'bg-green-400'
+                  : word.includes(guess[i]) 
+                  ? 'bg-yellow-400' 
+                  : 'bg-black'
+                return(
+                  <div className="h-16 w-16 border border-gray-400 bg-zinc-600 text-white font-bold uppercase flex items-center justify-center">
+                      {guess[i]}
+                      </div>
+                      )
+        })}
+    </div>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,12 @@ import PuzzleStore from "../stores/PuzzleStore";
 export default observer(function Home() {
   const store = useLocalObservable(() => PuzzleStore)
 
-  return <div>
-    <h1>Wordle</h1>
-    <Guess/>
+  return <div className="flex h-screen w-screen items-center justify-center flex-col bg-zinc-400 ">
+    <h1 className="text-6xl font-bold uppercasr text-transparent bg-clip-text bg-gradient-to-br from-red-800 to-red-800">Wordle</h1>
+    {new Array(6).fill(0).map((_,i) => (
+      <Guess word={"test"} guess={"guess"} isGuessed={false}/>
+    ))}
+    
     <h1>won/loss</h1>
     <Querty/>
   </div>
