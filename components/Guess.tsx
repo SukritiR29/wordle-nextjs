70170e4e22dc467d4af1b868638413952e7ceeec
SukritiR29 <sukritirajora@gmail.com>
2024-01-02 17:09:19 +0530

    guess update

--- components/Guess.tsx
+++ components/Guess.tsx
@@ -3,16 +3,16 @@ export default function Guess({ isGuessed, guess, word }) {
       <div className="mb-2 grid grid-cols-5 gap-2">
         {new Array(5).fill(0).map((_, i) => {
           const bgColor = !isGuessed
-            ? 'bg-black'
+            ? 'bg-cyan-50 text-slate-700'
             : guess[i] === word[i]
-            ? 'bg-green-600'
+            ? 'bg-green-400 text-slate-50'
             : word.includes(guess[i])
-            ? 'bg-yellow-400'
-            : 'bg-black'
+            ? 'bg-yellow-400 text-slate-50'
+            : 'bg-red-400 text-slate-50'
   
           return (
             <div
-              className={`flex h-16 w-16 items-center justify-center border border-gray-400 font-bold uppercase text-white ${bgColor}`}
+              className={`flex h-12 w-12 items-center justify-center border border-2 border-cyan-700 border-gray-400 font-bold uppercase text-black ${bgColor}`}
             >
               {guess[i]}
             </div>
